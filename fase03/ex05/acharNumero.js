a4241170ed634d73bf34f4aa32ec63f9f18a72e9
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 12:13:46 -0300

    05

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,19 @@
+function acharNumero(num){ 
+    var valor = num
+    if(num<5) {
+        return "mercurio"
+        
+    } 
+    else if (num >=5 && num <=10) { return "venus"
+        
+        
+    } 
+    
+    else if (num >10 && num <=100) {return "terra"
+}
+
+    else {return "marte"} 
+}
+
+
+    

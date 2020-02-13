8b4ea3aba1da501533176ece9ece5a7fdfd9978c
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 13:29:25 -0300

    00

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,7 +1,15 @@
-function entreNumeros(min,max){
-    while(min <= max){
-        console.log(min,max)
-        min++
+function fizzbuzz(num1, num2){
+    while(num1 <= num2){
+        if(num1 % 3 === 0 && num1 % 5 === 0){
+            console.log(num1 + " FizzBuzz");
+        }else if(num1 % 5 === 0){
+            console.log(num1 + " Buzz");
+        }else if(num1 % 3 === 0){
+            console.log(num1 + " Fizz");
+        }else{
+            console.log(num1);
+        }
+        num1++
     }
 }
 

2eca238e17f01b3f0026a0bf46bc415f8ce73ab1
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 13:29:51 -0300

    02

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,14 +1,15 @@
 function fizzbuzz(num1, num2){
     while(num1 <= num2){
         if(num1 % 3 === 0 && num1 % 5 === 0){
-            console.log(num1 + " FizzBuzz ");
+            console.log(num1 + " FizzBuzz");
         }else if(num1 % 5 === 0){
-            console.log(num1 + " Buzz ");
+            console.log(num1 + " Buzz");
         }else if(num1 % 3 === 0){
-            console.log(num1 + " Fizz ");
+            console.log(num1 + " Fizz");
         }else{
             console.log(num1);
         }
         num1++
     }
 }
+

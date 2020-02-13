b7910647806002a81006bdfd24786aa1e417d196
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 13:05:18 -0300

    02

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,14 @@
+function fizzbuzz(num1, num2){
+    while(num1 <= num2){
+        if(num1 % 3 === 0 && num1 % 5 === 0){
+            console.log(num1 + " FizzBuzz ");
+        }else if(num1 % 5 === 0){
+            console.log(num1 + " Buzz ");
+        }else if(num1 % 3 === 0){
+            console.log(num1 + " Fizz ");
+        }else{
+            console.log(num1);
+        }
+        num1++
+    }
+}

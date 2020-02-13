320864bdfbe1fcd908056d147c9c39ea0446abeb
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 13:38:47 -0300

    03

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,15 +1,18 @@
 function por42(num1, num2){
 var result = null;
+var a = 0
 while(num1 <= num2){
     if(num1 % 42 === 0){
-        return Math.max(num1) + 42;
+        a = a + 1
     }
-    num1++;
+if(a === 2){
+    return(num1)
 }
-    if(!result){
+num1++
+}
+    if(!result)
         console.log("Não encontrado");
         return false;
-}
 }
 
 

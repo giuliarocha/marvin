d1bb1f85b97fd89607dffef21f26828f08e51935
giuliarocha <60925428+giuliarocha@users.noreply.github.com>
2020-02-13 13:18:29 -0300

    01

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,13 @@
+function ateZero(numero){
+    var i = 0;
+    while(i <= numero){
+        console.log(i)
+        i++
+    }
+    if(numero <= 0){
+        while (i >= numero){
+            console.log(numero)
+            numero++
+        }
+    }
+}
